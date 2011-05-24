39053e4aa2aed9b5e63dfc012f99e260e5223287
Josh Leven <josh.leven@goalpostlabs.com>
2011-05-24 13:51:26 -0400

    changed railtie to trigger after db connection so that the correct adapters can be modified without having to specifically require all possible adapters

--- lib/presenter-pattern/railtie.rb
+++ lib/presenter-pattern/railtie.rb
@@ -1,46 +1,48 @@
 require 'rails/railtie'
 
-require 'active_record'
-require 'active_record/connection_adapters/postgresql_adapter'
-require 'active_record/connection_adapters/mysql_adapter'
-require 'active_record/connection_adapters/sqlite_adapter'
 
 module PresenterPattern
   class IllegalDatabaseQueryFromView < RuntimeError; end
 
   class Railtie < Rails::Railtie
-    initializer "presenter_pattern.raise_on_view_query" do |app|
+    config.after_initialize do
+
       if Rails.env.development? || Rails.env.test?
-        puts "[WARNING] Presenter::Rails is preventing your views from triggering PG, MySQL, SQLite3 or SQLite database connections."
 
+        adapters = begin
+          ::ActiveRecord::ConnectionAdapters::AbstractAdapter.subclasses
+        rescue
+          #ActiveRecord not loaded
+          []
+        end
         
-        ::ActiveRecord::ConnectionAdapters.class_eval do
+        if adapters.any?
 
-          %w(PostgreSQLAdapter MysqlAdapter SQLiteAdapter).each do |adapter_name|
-            begin
-              adapter = const_get adapter_name
-              adapter.class_eval do
-                alias :orig_execute :execute
-                def execute(*args)
-                  first_view   = caller.grep(/app\/views/).first
-                  #first_helper = caller.grep(/app\/helpers/).first
+          puts "\n[WARNING] PresenterPattern is preventing your views from triggering a database query through the following adapters:"
+          adapters.map{|a| a.name.split('::').last}.each {|name| puts "  * #{name}"}
+          puts
 
-                  # if we're coming from a view, let's analyze the situation
-                  if !first_view.nil? #and (first_helper.nil? or (caller.index(first_view) < caller.index(first_helper)))
-                    raise PresenterPattern::IllegalDatabaseQueryFromView, "No query from view prohibited, eager-load from a controller instead."
-                  else
-                    orig_execute *args
-                  end
+          adapters.each do |adapter|
+            adapter.class_eval do
+              alias :orig_execute :execute
+              def execute(*args)
+                first_view   = caller.grep(/app\/views/).first
+                #first_helper = caller.grep(/app\/helpers/).first
+
+                # if we're coming from a view, let's analyze the situation
+                if !first_view.nil? #and (first_helper.nil? or (caller.index(first_view) < caller.index(first_helper)))
+                  raise PresenterPattern::IllegalDatabaseQueryFromView, "No query from view prohibited, eager-load from a controller instead."
+                else
+                  orig_execute *args
                 end
               end
-            rescue
-              p "the adapter #{adapter_name} has not been instantiated"
             end
           end
+          
         end
 
-
       end
+      
     end
   end
 end
